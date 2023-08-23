df2299fae2c235598ab5d1a501d526dc386806b4
KexySerJ90 <exserj9011@gmail.com>
2023-08-23 17:18:23 +0300

    beta version

--- main.py
+++ main.py
@@ -4,23 +4,34 @@ from random import choice
 BACKGROUND_COLOR = "#B1DDC6"
 
 
-window = Tk()
-window.title('Flash card')
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 data=pandas.read_csv('data/russian_english_adjectives.csv')
 to_learn=data.to_dict(orient="records")
+cur_card={}
 
 def next_card():
-    cur_card=choice(to_learn)
-    canvas.itemconfig(card_title,text="English")
-    canvas.itemconfig(card_word,text=cur_card["English"])
+    global cur_card, flip_timer
+    window.after_cancel(flip_timer)
+    cur_card.update(choice(to_learn))
+    canvas.itemconfig(card_title,text="English",fill='black')
+    canvas.itemconfig(card_word,text=cur_card["English"],fill='black')
+    canvas.itemconfig(canvas_image, image=card_img)
+    flip_timer = window.after(3000, flip_card)
 
+def flip_card():
+    canvas.itemconfig(card_title, text="Russian",fill='white')
+    canvas.itemconfig(card_word, text=cur_card["Russian"],fill='white')
+    canvas.itemconfig(canvas_image, image=card_back_img)
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Flash card')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer= window.after(3000, flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_img = PhotoImage(file='images/card_front.png')
-canvas.create_image(400, 263, image=card_img)
+card_back_img=PhotoImage(file='images/card_back.png')
+canvas_image=canvas.create_image(400, 263, image=card_img)
 card_title=canvas.create_text(400, 150, text='', fill='black',font=('Ariel', 40, 'italic'))
 card_word=canvas.create_text(400, 263, text='', fill='black',font=('Ariel', 60, 'bold'))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
@@ -32,5 +43,6 @@ wi=PhotoImage(file='images/wrong.png')
 wrong_button = Button(image=wi, highlightthickness=0,borderwidth=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 next_card()
+
 
 window.mainloop()
